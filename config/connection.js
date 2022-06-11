f6d26f9b5ad286d38279697cfa3168bedd450b37
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-11 21:08:31 +1000

    added code for connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,19 @@
 requires('dotenv').config()
+const Sequelize = require('sequelize')
+
+let sqlModel
+
+process.env.JAWSDB_URL ? 
+sqlModel = new Sequelize(process.env.JAWSDB_URL) : 
+sqlModel = new Sequelize(
+	process.env.db_name,
+	process.env.db_user,
+	process.env.db_password,
+	{
+		host: 'localhost',
+		dialect: 'mysql',
+		port: 3306
+	}
+)
+
+module.exports = sqlModel
