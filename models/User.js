c67c89e7b44ec73e57ada880ec995b836727a543
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-13 20:34:58 +0930

    added model for user

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const{Model, DataTypes} = require('sequelize')
+const sequelize = require('../config/connection')
+
+class User extends Model {}
+
+User.init(
+	{
+		username:{
+			type: DataTypes.STRING,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: false
+		},
+		password:{
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	},
+	{
+		sequelize,
+		timestamp:true,
+		freezeTableName: true,
+		underscore:true,
+		modelName:'user'
+	}
+)
+
+module.exports = User
