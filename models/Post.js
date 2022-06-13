8ce7654119b264cc255b1f9e16fd639d3091e596
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-13 20:42:36 +0930

    added code for 'post' model

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const{Model, DataTypes} = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Post extends Model {}
+
+Post.init(
+	{
+		id:{
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		user_id:{
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		content:{
+			type: DataTypes.TEXT,
+			allowNull: true
+		}
+	},
+	{
+		sequelize,
+		timestamp:true,
+		freezeTableName: true,
+		underscore: true,
+		modelName:'post'
+	}
+)
+
+module.exports = Post
