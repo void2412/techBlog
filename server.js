31ab6e4e7030ed2376d94fe6b8aeb4841eb95f3e
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-18 06:51:40 +0930

    finished server.js

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
 const {Comment, Post, User} = require('./models/relationship')
-// const handlebars = require('express-handlebars')
+const route = require('./controllers')
+
+const handlebars = require('express-handlebars')
 const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express()
 const port = process.env.PORT || 3001
 
-// const hbs = handlebars.create({})
+const hbs = handlebars.create({})
 
+const sess = {
+	secret: process.env.session_secret,
+	cookie: {
+		maxAge: 1000 * 60 * 3
+	},
+	resave: false,
+	saveUninitialized: false,
+	store: new SequelizeStore({
+		db:sequelize
+	})
+}
 
+app.use(session(sess))
 
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(route)
 
 
 sequelize.sync({force: false}).then(() => {
