fb295bfef9ac9757adff26a44962e809f524a5ef
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-19 19:46:46 +0930

    added logout route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,7 +45,16 @@ router.post('/signup', async (req, res) => {
 	}
 })
 
-
+router.post('/logout', (req, res)=>{
+	if(req.session.logged_in){
+		req.session.destroy(()=>{
+			res.status(204).end()
+		})
+	}
+	else{
+		res.status(404).end()
+	}
+})
 
 
 
